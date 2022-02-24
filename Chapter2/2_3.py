3767b764c1c6486d0e7b3c6ccfc2b644ee2a42cc
CreateETC10 <greene_10@mail.fresnostate.edu>
2022-02-24 15:28:59 -0800

    changes

---
+++ Chapter2/2_3.py
@@ -0,0 +1,12 @@
+print("one","two","three",sep=",")
+
+print("Hello",end="")
+print("World!")
+
+print("a\tb\tc") # a    b   c
+print("a\tb\tc\nd\te\tf") #a    b   c
+                          #d    e   f
+
+print("\"Hello World!\"") #"Hello World!"
+
+print("")
