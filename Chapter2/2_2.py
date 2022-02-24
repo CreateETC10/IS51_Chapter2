7f59f15291153ae7342679341e90ada9a74cbd1d
CreateETC10 <greene_10@mail.fresnostate.edu>
2022-02-24 14:50:39 -0800

    intitial commit

---
+++ Chapter2/2_2.py
@@ -0,0 +1,38 @@
+# # town= input("Enter the town of your city")
+# full_name=input("Enter a full name")
+# n=full_name.rfind("")
+# print("Last name:", full_name[n+1:])
+# print("First name(s):", full_name[:n])
+
+
+# Joseph Greene
+# full_name=input("Enter your full name: ")
+# index_of_space=full_name.rfind(" ")
+# first_name= full_name[:index_of_space]
+# last_name=full_name[index_of_space+1:]
+# print("first_name", first_name)
+# print("Last name", last_name)
+
+
+# annual_income=eval(input("enter your annual income: "))
+# print("Users income is", annual_income)
+# monthly_income=annual_income/12
+# print("monthly_income",monthly_income)
+
+# var1=1
+# var2=2
+# var3="3"
+# var4="4"
+
+# var5=var1+var2 # 3 Int
+# var6=var3+var4 # 34 string
+
+str1="Python"
+length=len(str1) #5 int
+str2=str1[:] # python string
+print("str2", str2)
+str3=str1[:10] # python
+print("str3",str3) 
+str4=str2[2:-1]
+print("str4",str4)
+
