257c429c071d1050deb276997c39fd071cd2fc99
CreateETC10 <greene_10@mail.fresnostate.edu>
2022-03-01 14:54:42 -0800

    finish 2_3

--- Chapter2/2_3.py
+++ Chapter2/2_3.py
@@ -9,8 +9,23 @@
 
 # print("\"Hello World!\"") #"Hello World!"
 
-print("0123456789012345678901234567")
-print("Rank".1just(5), "Player".1just(20), "HR".rjust(3), sep=" ")
-print('1'.center(5), "Barry Bonds".1just(20), "762".rjust(3), sep="")
-print('2'.center(5), "Hank Aaron".1just(20), "755".rjust(3), sep="")
-print('3'.center(5), "Babe Ruth".1just(20), "714".rjust(3), sep="")
+# print("0123456789012345678901234567")
+# print("Rank".ljust(5), "Player".ljust(20), "HR".rjust(3), sep=" ")
+# print('1'.center(5), "Barry Bonds".ljust(20), "762".rjust(3), sep="")
+# print('2'.center(5), "Hank Aaron".ljust(20), "755".rjust(3), sep="")
+# print('3'.center(5), "Babe Ruth".ljust(20), "714".rjust(3), sep="")
+
+# str1 ="There is {0}% probability that the stock market will crash tomorrow and {1}% probability that it will rally!"
+# print(str1.format(10,50))
+
+# print('2'.center(5), "Hank Aaron".ljust(20), "755".rjust(3), sep="")
+
+
+# print("012345678901234567801234567")
+# print("{0:^5s}{1:<20s}{2:>3s}".format("Rank","Player","HR"))
+# print("{0:^5n}{1:<20s}{2:>3n}".format(1,"Barry Bonds",762))
+
+print("The area of {0:s} is {1:,d} square miles".format("Texas",268820))
+str1="The population of {0:s} is {1:.2%} of the U.S. population."
+print(str1.format("Texas", 26448000/309000000))
+
