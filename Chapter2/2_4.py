d606f252f4e48110ac5af42c17b01731bb6017ae
CreateETC10 <greene_10@mail.fresnostate.edu>
2022-03-08 14:23:04 -0800

    Changes

--- Chapter2/2_4.py
+++ Chapter2/2_4.py
@@ -1,7 +1,12 @@
-num1=1
-num2=10
-num3=1000
-sum_of_all_numbers=num1+num2+num3
-print("Sum is {}0:n".format(sum_of_all_numbers))
-list_of_numbers=[1,10,1000]
-print(sum(list_of_numbers))
+# num1=1
+# num2=10
+# num3=1000
+# sum_of_all_numbers=num1+num2+num3
+# print("Sum is {}0:n".format(sum_of_all_numbers))
+# list_of_numbers=[1,10,1000]
+# print(sum(list_of_numbers))
+
+
+team=["Seahawks",2014, "CenturyLink Field"]
+nums=[5, 10, 4, 5]
+words=["spam","ni"]
