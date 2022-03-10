951f3d566afce98f48754b845b1b16b82a18306f
CreateETC10 <greene_10@mail.fresnostate.edu>
2022-03-10 15:07:33 -0800

    class 3/10

--- Chapter2/2_4.py
+++ Chapter2/2_4.py
@@ -30,23 +30,55 @@
 # words.insert(1,"Hello")
 # print("Words======>", words)
 
-grades=[]
-num=float(input("Enter the first grade"))
-grades.append(num)
-num=float(input("Enter the second grade"))
-grades.append(num)
-num=float(input("Enter the third grade"))
-grades.append(num)
-num=float(input("Enter the fourth grade"))
-grades.append(num)
-num=float(input("Enter the fifth grade"))
-grades.append(num)
+# grades=[]
+# num=float(input("Enter the first grade"))
+# grades.append(num)
+# num=float(input("Enter the second grade"))
+# grades.append(num)
+# num=float(input("Enter the third grade"))
+# grades.append(num)
+# num=float(input("Enter the fourth grade"))
+# grades.append(num)
+# num=float(input("Enter the fifth grade"))
+# grades.append(num)
 
 
-grades.append(num)
-minimum_grade=min(grades)
-grades.remove(minimum_grade)
-minimum_grade=min(grades)
-grades.remove(minimum_grade)
-average=sum(grades)/len(grades)
-print("Average Grade: {0:.2f}".format(average))
+# grades.append(num)
+# minimum_grade=min(grades)
+# grades.remove(minimum_grade)
+# minimum_grade=min(grades)
+# grades.remove(minimum_grade)
+# average=sum(grades)/len(grades)
+# print("Average Grade: {0:.2f}".format(average))
+
+
+# str1="a,b,c"
+# parts=str1.split(",")
+# print("parts:",parts)
+
+# print("a,b,c".split(','))
+# print("a**b**c".split(','))
+# print("a\nb\nc".split())
+# print("a b c".split())
+
+# lines= ["to","be","or","not","to","be"]
+# print("Lines before join:",lines)
+# joined="".join(lines)
+# print("joined:",joined)
+
+# names=[]
+# infile=open("data.txt",'r')
+# print("infile",infile)
+# for line in infile:
+#     # print("line",line)
+#     names.append(line.rstrip())
+# print("names:",names)
+# names_using_line_comprehension=[line.rstrip()for line in infile]
+# print("names_using_list_comprehension:",names_using_line_comprehension)
+# infile.close()
+
+# infile=open("Grades.txt", "r")
+# for line in infile:
+#     print("line:", line)
+# infile.close()
+
